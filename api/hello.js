12d83fee5350ff4b997b366df86dbf3a31e39cc2
UPNOD <admin@upnod.tv>
2025-02-19 17:31:59 +0700

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,3 @@
-module.exports = (req, res) => {
-  res.status(200).json({ message: "Hello from Vercel API!" });
-};
+export default function handler(req, res) {
+  res.status(200).json({ message: "Hello, world!" });
+}
